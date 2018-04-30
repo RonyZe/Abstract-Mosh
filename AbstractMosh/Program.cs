51edef4f7ea1868bf83073affefc4a3133787396
Rehan <zrony58@gmail.com>
2018-04-30 23:49:10 +0500

    Initial Commit

---
+++ AbstractMosh/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractMosh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //            var shape = new Shape();
+
+            var circle=new Circle();
+            circle.Draw();
+
+            var rect=new Rectangle();
+            rect.Draw();
+            Console.ReadLine();
+        }
+    }
+
+
+}
